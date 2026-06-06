690052d103db240e4c4b5fe3628f1260e16690ef
DarllCHL <tu@email.com>
2026-06-06 10:18:41 -0400

    fix: agregar datos iniciales en import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO student (name) VALUES ('Juan');
+INSERT INTO student (name) VALUES ('Maria');
+INSERT INTO student (name) VALUES ('Carlos');
+INSERT INTO student (name) VALUES ('Ana');
+INSERT INTO student (name) VALUES ('Pedro');
